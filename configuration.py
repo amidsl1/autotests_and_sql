6abb40129bc4737841a218af2c2a41639784a7fb
amidsl1 <attackattack1@yandex.ru>
2024-02-25 01:52:30 +0200

    Commit

---
+++ configuration.py
@@ -0,0 +1,3 @@
+URL_SERVICE = "https://411c8f9f-7ba5-4e25-9ebd-ef4bd344643b.serverhub.praktikum-services.ru"
+CREATE_NEW_ORDER = "/api/v1/orders"
+RECEIVING_ORDER_BY_ID = "/api/v1/orders/track"
---
+++ get_order_track_info.py
@@ -0,0 +1,8 @@
+# Дмитрий Слинчук, 13 - ая когорта, финальный проект. Инженер по тестированию плюс
+import request
+
+def test_order_creation():
+    creation_response = request.post_new_order()
+    track_id = creation_response.json()['track']
+    response = request.get_orders_track(track_id)
+    assert response.status_code == 200
